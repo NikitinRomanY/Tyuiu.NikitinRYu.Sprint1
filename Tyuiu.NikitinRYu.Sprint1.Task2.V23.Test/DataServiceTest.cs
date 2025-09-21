df15c537af659fde76bac15ad74ec2605ac653ac
Tyuiu.NikitinRy@outlook.com <Tyuiu.NikitinRy@outlook.com>
2025-09-21 15:18:14 +0500

    Task2

---
+++ Tyuiu.NikitinRYu.Sprint1.Task2.V23.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.NikitinRYu.Sprint1.Task2.V23.Lib;
+
+namespace Tyuiu.NikitinRYu.Sprint1.Task2.V23.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            int x = 6;
+            var res = ds.ConvertMinutesToSeconds(x);
+            Assert.AreEqual(360, res);
+        }
+    }
+}
---
+++ Tyuiu.NikitinRYu.Sprint1.Task2.V23/Program.cs
@@ -0,0 +1,32 @@
+﻿using Tyuiu.NikitinRYu.Sprint1.Task2.V23.Lib;
+
+DataService ds = new DataService();
+
+Console.Title = "Спринт #1 | Выполнил: Никитин Р.Ю. | ИБКСб-25-1";
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #1                                                               *");
+Console.WriteLine("* Тема: Арифметические операторы в C#                                     *");
+Console.WriteLine("* Задание #2                                                              *");
+Console.WriteLine("* Вариант #23                                                             *");
+Console.WriteLine("* Выполнил: Никитин Роман Юрьевич     | ИБКСб-25-1                        *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране.             *");
+Console.WriteLine("*                                                                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+int x;
+
+Console.WriteLine("Введите время в секундах X (целое число):");
+x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine("Время в секундах  = " + ds.ConvertMinutesToSeconds(x));
+Console.ReadLine();
