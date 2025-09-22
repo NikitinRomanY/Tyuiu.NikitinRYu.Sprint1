df0d0daf10f23c53235d1325aa12712576e4c399
Tyuiu.NikitinRy@outlook.com <Tyuiu.NikitinRy@outlook.com>
2025-09-22 13:49:05 +0500

    Task3

---
+++ Tyuiu.NikitinRYu.Sprint1.Task3.V3.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.NikitinRYu.Sprint1.Task3.V3.Lib;
+
+namespace Tyuiu.NikitinRYu.Sprint1.Task3.V3.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double length = 9;
+            double width = 7.5;
+            double height = 5;
+            var res = ds.ParallelepipedVolume(length, width, height);
+            Assert.AreEqual(337.5, res);
+
+        }
+    }
+}
---
+++ Tyuiu.NikitinRYu.Sprint1.Task3.V3/Program.cs
@@ -0,0 +1,42 @@
+﻿using Tyuiu.NikitinRYu.Sprint1.Task3.V3.Lib;
+
+DataService ds = new DataService();
+
+Console.Title = "Спринт #1 | Выполнил: Никитин Р.Ю. | ИБКСб-25-1";
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #1                                                               *");
+Console.WriteLine("* Тема: Операторы составного присваивания                                 *");
+Console.WriteLine("* Задание #3                                                              *");
+Console.WriteLine("* Вариант #3                                                              *");
+Console.WriteLine("* Выполнил: Никитин Роман Юрьевич     | ИБКСб-25-1                        *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране.             *");
+Console.WriteLine("*                                                                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+double length;
+
+Console.WriteLine("Введите длинну:");
+length = Convert.ToDouble(Console.ReadLine());
+
+double width;
+
+Console.WriteLine("Введите ширину:");
+width = Convert.ToDouble(Console.ReadLine());
+
+double height;
+
+Console.WriteLine("Введите высоту:");
+height = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine("Одъем в кубических сантиметрах  = " + ds.ParallelepipedVolume(length, width, height));
+Console.ReadLine();
