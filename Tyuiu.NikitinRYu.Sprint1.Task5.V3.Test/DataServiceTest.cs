d03c34124a3617a77cb9cb7e52a42c14a317280d
Tyuiu.NikitinRy@outlook.com <Tyuiu.NikitinRy@outlook.com>
2025-09-24 20:41:18 +0500

    Task5

---
+++ Tyuiu.NikitinRYu.Sprint1.Task5.V3.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.NikitinRYu.Sprint1.Task5.V3.Lib;
+
+namespace Tyuiu.NikitinRYu.Sprint1.Task5.V3.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            int k = 130985;
+            int h = ds.Calculate(k);
+            int wait = 9;
+            Assert.AreEqual(wait, h);
+        }
+    }
+}
---
+++ Tyuiu.NikitinRYu.Sprint1.Task5.V3/Program.cs
@@ -0,0 +1,40 @@
+﻿using Tyuiu.NikitinRYu.Sprint1.Task5.V3.Lib;
+
+DataService ds = new DataService();
+
+Console.Title = "Спринт #1 | Выполнил: Никитин Р.Ю. | ИБКСб-25-1";
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #1                                                               *");
+Console.WriteLine("* Тема: Преобразование типов и класс Convert                              *");
+Console.WriteLine("* Задание #5                                                              *");
+Console.WriteLine("* Вариант #3                                                              *");
+Console.WriteLine("* Выполнил: Никитин Роман Юрьевич     | ИБКСб-25-1                        *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Написать программу, которая присваивает целой переменной h третью от    *");
+Console.WriteLine("* конца цифру в записи положительного целого числа k                      *");
+Console.WriteLine("*                                                                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+Console.Write("Введите положительное целое число k: ");
+string input = Console.ReadLine();
+
+if (!int.TryParse(input, out int k) || k <= 0)
+{
+    Console.WriteLine("Ошибка: введите положительное целое число!");
+    Console.ReadKey();
+    return;
+}
+
+int h = ds.Calculate(k);
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine($"Третья с конца цифра числа {k} равна {h}");
+
+Console.ReadKey();
